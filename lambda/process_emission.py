b18f73bc8e9514415a88bcabc6f9b0860d22cfdf
crv4 <crv4@illinois.edu>
2024-04-03 20:17:08 -0500

    Lambda Boilerplate

---
+++ lambda/process_emission.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import sys
+
+import greengrasssdk
+
+# number of devices
+device_count = settings.DEVICE_COUNT
+#Path to the dataset, modify this
+data_path = "data/vehicle{}.csv"
+
+# Logging
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+
+# SDK Client
+client = greengrasssdk.client("iot-data")
+
+# Counter
+my_counter = 0
+def lambda_handler(event, context):
+    global my_counter
+    #TODO1: Get your data
+
+    #TODO2: Calculate max CO2 emission
+
+    #TODO3: Return the result
+    client.publish(
+        topic="hello/world/counter",
+        payload=json.dumps(
+            {"message": "Hello world! Sent from Greengrass Core.  Invocation Count: {}".format(my_counter)}
+        ),
+    )
+    my_counter += 1
+
+    return
